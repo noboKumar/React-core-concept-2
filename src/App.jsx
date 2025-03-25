4ca4610438ebf625a1224e546b8d5ab55473b5a4
noboKumar <nobokumar227@gmail.com>
2025-03-25 12:21:52 +0600

    add title

--- src/App.jsx
+++ src/App.jsx
@@ -30,14 +30,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React-core-concept-part-2</h1>
 
       <button
         onClick={() => {
